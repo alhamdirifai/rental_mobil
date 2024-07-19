738f348c6fa0c347297b389168e17e80f2363efa
alhamdirifai <109028286+alhamdirifai@users.noreply.github.com>
2024-07-20 04:44:39 +0700

    Delete hmobil.php

--- hmobil.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include("konek.php");
-$id=$_GET['id'];
-$hapus = mysqli_query($konek, "delete from mobil where id_mobil='$id'");
-if ($hapus){
-    header("Location: index.php?x=mobil");
-}
